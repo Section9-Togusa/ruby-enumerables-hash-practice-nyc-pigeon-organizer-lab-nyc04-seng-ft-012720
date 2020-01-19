c80fe8fc2d9ae74f89e232e635efde650f0f609b
Daryl Corbin <corbindac@gmail.com>
2020-01-19 17:36:22 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,40 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeon_list = {
+   "Theo" => {
+     :color => ["purple", "grey"],
+     :gender => "male",
+     :lives => "Subway"
+   },
+   "Peter Jr." => {
+     :color => ["purple", "grey"],
+     :gender => "male",
+     :lives => "Library"
+   },
+   "Lucky" => {
+     :color => ["purple"],
+     :gender => "male",
+     :lives => "City Hall"
+   },
+   "Ms .K" => {
+     :color => ["grey", "white"],
+     :gender => "female",
+     :lives => "Central Park"
+   },
+   "Queenie" => {
+     :color => ["white", "brown"],
+     :gender => "female",
+     :lives => "Subway"
+   },
+   "Andrew" => {
+     :color => ["white"],
+     :gender => "male",
+     :lives => "Central Park"
+   },
+   "Alex" => {
+     :color => ["white", "brown"],
+     :gender => "male",
+     :lives => "Central Park"
+   }
+ }
 end
